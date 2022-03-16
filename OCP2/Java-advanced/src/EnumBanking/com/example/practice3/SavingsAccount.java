b17bb704515cad69fc3f197ff606ec9a350633ad
ConradMar3 <96224782+ConradMar3@users.noreply.github.com>
2022-03-16 19:37:51 +0200

    2022/03/16 (pm)
    Chapter5-practice3

---
+++ OCP2/Java-advanced/src/EnumBanking/com/example/practice3/SavingsAccount.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EnumBanking.com.example.practice3;
+
+
+public class SavingsAccount extends Account{
+     Double rateofinterest=0.06;
+
+    public SavingsAccount(double balance) {
+        super(balance);
+    }
+
+    
+    
+ 
+    
+
+    @Override
+    public boolean withdraw(double amount) {
+        if(amount <= balance ) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    
+     @Override
+     public void  deposit(double amount) {
+       balance += amount;
+       balance +=balance * rateofinterest;
+       
+        }
+    
+    @Override
+    public String getDescription() {
+        return "Savings Account";
+    }
+    
+}
