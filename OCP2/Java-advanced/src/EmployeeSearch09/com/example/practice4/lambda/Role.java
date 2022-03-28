fd36799d223ca3008b7e4d39da24cc542f0781dd
ConradMar3 <conradmare890317@gmail.com>
2022-03-28 14:34:15 +0200

    2022/03/28 (pm)
    Chapter9 complete.

---
+++ OCP2/Java-advanced/src/EmployeeSearch09/com/example/practice4/lambda/Role.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EmployeeSearch09.com.example.practice4.lambda;
+
+/**
+ * @author MikeW
+ */
+public enum Role { STAFF, MANAGER, EXECUTIVE }
