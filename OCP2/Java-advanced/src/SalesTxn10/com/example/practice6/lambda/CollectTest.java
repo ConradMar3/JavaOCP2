08c9ad06c9bfd5f98394d96110896ea00860c300
ConradMar3 <conradmare890317@gmail.com>
2022-03-28 15:44:43 +0200

    2022/03/28 (pm)
    Practice6.

---
+++ OCP2/Java-advanced/src/SalesTxn10/com/example/practice6/lambda/CollectTest.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SalesTxn10.com.example.practice6.lambda;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author oracle
+ */
+public class CollectTest {
+    
+    public static void main(String[] args) {
+
+        List<SalesTxn> tList = SalesTxn.createTxnList();
+        List<SalesTxn> rList;
+        
+        // Print out Transactions over $300k
+        System.out.println("=== Transactions over $300k ===");
+        rList = tList.stream()
+            .filter(t -> t.getTransactionTotal() > 300000)
+            .sorted(Comparator.comparing(SalesTxn::getTransactionTotal))
+            .collect(Collectors.toList());
+            
+        
+        rList.stream()
+            .forEach( t -> 
+                System.out.printf(
+                    "Id: " + t.getTxnId() + " Seller: " + t.getSalesPerson() + 
+                    " Buyer: " + t.getBuyerName() +
+                    " Amt: $%,9.2f%n", t.getTransactionTotal()
+                )
+            );
+        
+        
+    }
+}
---
+++ OCP2/Java-advanced/src/SalesTxn10/com/example/practice6/lambda/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SalesTxn10.com.example.practice6.lambda;
+/**
+ *
+ * @author MikeW
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    
+      CollectTest.main(args);
+      
+  }
+}
