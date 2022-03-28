99adf467ae2ba392b0c416eb3888e4a6a979def0
ConradMar3 <conradmare890317@gmail.com>
2022-03-28 15:29:35 +0200

    2022/03/28 (pm)
    Practice4 & 5.

---
+++ OCP2/Java-advanced/src/SalesTxn10/com/example/practice4/lambda/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SalesTxn10.com.example.practice4.lambda;
+
+/**
+ *
+ * @author MikeW
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    
+      CalcTest.main(args);
+      
+  }
+}
---
+++ OCP2/Java-advanced/src/SalesTxn10/com/example/practice5/lambda/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SalesTxn10.com.example.practice5.lambda;
+
+/**
+ *
+ * @author MikeW
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    
+      SortTest.main(args);
+      
+  }
+}
