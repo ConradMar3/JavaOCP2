a5d49f344be9c8591e6b10b3d009aeba22c0574b
ConradMar3 <conradmare890317@gmail.com>
2022-03-28 16:18:16 +0200

    2022/03/28 (pm)
    Custom Exception Example.

---
+++ OCP2/Java-advanced_ig2/src/CustomExceptionExample/com/example/CustomExceptionExample.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CustomExceptionExample.com.example;
+
+
+public class CustomExceptionExample {
+
+    public static void main(String[] args) {
+
+        try {
+
+            CustomExceptionExample.checkPassWord("pass");
+
+        } catch (InvalidPasswordException e) {
+
+            e.printStackTrace();
+
+        }
+
+        try {
+
+            CustomExceptionExample.checkPassWord(null);
+
+        } catch (InvalidPasswordException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static void checkPassWord(String pass) throws InvalidPasswordException {
+
+        int minPassLength = 5;
+
+        try {
+
+            if (pass.length() < minPassLength) {
+                throw new InvalidPasswordException("The password provided is too short");
+            }
+
+        } catch (NullPointerException e) {
+
+            throw new InvalidPasswordException("No password provided", e);
+
+        }
+    }
+}
+
+
+class InvalidPasswordException extends Exception {
+
+    InvalidPasswordException() {
+    }
+
+    InvalidPasswordException(String message) {
+
+        super(message);
+    }
+
+    InvalidPasswordException(String message, Throwable cause) {
+
+        super(message, cause);
+    }
+}
