d7b561e2dc5427a28746d33b161d0c2c21269461
ConradMar3 <conradmare890317@gmail.com>
2022-03-29 14:18:41 +0200

    2022/03/29 (pm)
    Custom Exceptions - practice2

---
+++ OCP2/Java-advanced_ig2/src/CustomExceptions11/com/example/practice2/Employee.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CustomExceptions11.com.example.practice2;
+
+import java.text.NumberFormat;
+
+
+
+public class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private float salary;
+
+    public Employee() { }
+    
+    public Employee(int id, String firstName, String lastName, float salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+       
+        this.salary = salary;
+    }
+
+public int getId() {
+    return id;
+}
+
+public String getFirstName() {
+    return firstName;
+}
+
+public String getLastName() {
+    return lastName;
+}
+
+
+public float getSalary() {
+    return salary;
+}
+
+@Override
+public String toString() {
+    return "Employee ID:   " + getId() + "\n" +
+            "Employee Name: " + getFirstName() + " " + getLastName() + "\n" +
+            
+            "Salary:        " + NumberFormat.getCurrencyInstance().format((double)getSalary());
+}
+
+
+
+
+    
+}
---
+++ OCP2/Java-advanced_ig2/src/CustomExceptions11/com/example/practice2/InvalidOperationException.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CustomExceptions11.com.example.practice2;
+
+public class InvalidOperationException extends Exception {
+
+    public InvalidOperationException() {
+        super();
+    }
+
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+
+    public InvalidOperationException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
