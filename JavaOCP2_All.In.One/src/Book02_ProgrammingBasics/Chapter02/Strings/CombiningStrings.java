828519600ce706f3ad2f04f78990f69ea9fa8638
ConradMar3 <conradmare890317@gmail.com>
2022-07-01 08:52:17 +0200

    2022/07/01 (am)
    Strings.

---
+++ JavaOCP2_All.In.One/src/Book02_ProgrammingBasics/Chapter02/Strings/CombiningStrings.java
@@ -0,0 +1,11 @@
+package Book02_ProgrammingBasics.Chapter02.Strings;
+
+public class CombiningStrings {
+    public static void main(String[] args) {
+        String hello = "Hello, ";
+        String world = "World";
+        String greeting = hello + world;
+
+        System.out.println(greeting);
+    }
+}
---
+++ JavaOCP2_All.In.One/src/Book02_ProgrammingBasics/Chapter02/Strings/DeclaringAndInitializingStrings.java
@@ -0,0 +1,16 @@
+package Book02_ProgrammingBasics.Chapter02.Strings;
+
+public class DeclaringAndInitializingStrings {
+    public static void main(String[] args) {
+        String s;
+        s = "Hello World!";
+
+        //Other method:
+        String t = "Java.";
+
+        //Initializing an empty local string variable:
+        String v = " ";
+
+        System.out.println(s + " " + t + " " + v);
+    }
+}
