9eaf3ca172c2993111d807461a0fa5011edcbeb9
ConradMar3 <conradmare890317@gmail.com>
2022-07-01 17:29:33 +0200

    2022/07/01 (pm)
    If-Else Statements.

---
+++ JavaOCP2_All.In.One/src/Book02_ProgrammingBasics/Chapter04/MakingChoices/SimpleBooleanExpression/SimpleBoolean.java
@@ -0,0 +1,16 @@
+package Book02_ProgrammingBasics.Chapter04.MakingChoices.SimpleBooleanExpression;
+
+public class SimpleBoolean {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 10;
+        int k = 15;
+        double x = 5.0;
+        double y = 7.5;
+        double z = 12.3;
+
+        if(i == 5) {
+            System.out.println("i is 5.");
+        }
+    }
+}
