c11039ab350a20b3f03a5c39972f3fb2d6c5d617
ConradMar3 <conradmare890317@gmail.com>
2022-07-05 11:06:47 +0200

    2022/07/05 (am)
    Book 1 - Chapter03_Complete.

---
+++ JavaOCP2_All.In.One/src/Book1_WelcomeToJava/Chapter02/JavaCommandLine/RunningJava.java
@@ -0,0 +1,9 @@
+package Book1_WelcomeToJava.Chapter02.JavaCommandLine;
+
+public class RunningJava {
+    public static void main(String[] args) {
+        System.out.println("Running Java Apps after compiling.");
+    }
+}
+
+//Run = java RunningJava
