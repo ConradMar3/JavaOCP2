91489b7b94ec5595c90713beff1b58e482196b55
ConradMar3 <conradmare890317@gmail.com>
2022-07-06 11:37:49 +0200

    2022/07/06 (am)
    For-Loops

---
+++ JavaOCP2_All.In.One/src/Book02_ProgrammingBasics/Chapter05/Loops/ForLoops/CountingBackwards/CountDown.java
@@ -0,0 +1,10 @@
+package Book02_ProgrammingBasics.Chapter05.Loops.ForLoops.CountingBackwards;
+
+public class CountDown {
+    public static void main(String[] args) {
+
+        for (int count = 10; count >= 1; count--) {
+            System.out.println(count);
+        }
+    }
+}
---
+++ JavaOCP2_All.In.One/src/Book02_ProgrammingBasics/Chapter05/Loops/ForLoops/ForEvenCounter.java
@@ -0,0 +1,10 @@
+package Book02_ProgrammingBasics.Chapter05.Loops.ForLoops;
+
+public class ForEvenCounter {
+    public static void main(String[] args) {
+
+        for (int number = 2; number <= 20; number += 2)
+            System.out.print(number + " ");
+        System.out.println();
+    }
+}
