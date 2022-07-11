255fb250aaefed795786af65dd98f6a2a564d35e
ConradMar3 <conradmare890317@gmail.com>
2022-07-11 09:26:51 +0200

    2022/07/11 (am)
    Book2 - Chapter 07- Methods.
    Basic Method, Guessing Game using Method.

---
+++ JavaOCP2_All.In.One/src/Book02_ProgrammingBasics/Chapter07/Methods/BasicMethod/HelloWorldMethod.java
@@ -0,0 +1,10 @@
+package Book02_ProgrammingBasics.Chapter07.Methods.BasicMethod;
+
+public class HelloWorldMethod {
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ JavaOCP2_All.In.One/src/Book02_ProgrammingBasics/Chapter07/Methods/BasicMethod/HelloWorldMethod2.java
@@ -0,0 +1,12 @@
+package Book02_ProgrammingBasics.Chapter07.Methods.BasicMethod;
+
+public class HelloWorldMethod2 {
+    public static void sayHello() {
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
+
+//Works exactly the same as example 1.
