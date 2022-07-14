4e1b4e3ca0ce23d7ad1d377eb761b4ca0a37974d
ConradMar3 <conradmare890317@gmail.com>
2022-07-14 08:07:33 +0200

    2022/07/14 (am)
    Understanding Methods

---
+++ JavaOCP2_All.In.One/src/Book03_ObjectOrientatedProgramming/Chapter02/Methods/Fear.java
@@ -0,0 +1,14 @@
+package Book03_ObjectOrientatedProgramming.Chapter02.Methods;
+
+public class Fear {
+    public static void main(String[] args) {
+        String f = "Once more into the frey... ";
+        String e = "\n\tTo the last good fight I will ever know... ";
+        String a = "\n\t\tOn this day I shall live and die... ";
+        String r = "\n\t\t\tOn this day I shall live and die... ";
+
+        boolean fear = Boolean.parseBoolean((f + e + a + r)); {
+        if(fear == false);
+            System.out.println(f + e + a + r);}
+    }
+}
