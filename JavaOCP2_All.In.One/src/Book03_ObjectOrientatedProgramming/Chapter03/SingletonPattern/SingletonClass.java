dff1992d3c0c7145b465bb23fb8031cd5876b66a
ConradMar3 <conradmare890317@gmail.com>
2022-07-15 12:14:17 +0200

    2022/07/15 (am)
    Book 3 - Chapter03.
    Initializers == Complete.

---
+++ JavaOCP2_All.In.One/src/Book03_ObjectOrientatedProgramming/Chapter03/SingletonPattern/SingletonClass.java
@@ -0,0 +1,24 @@
+package Book03_ObjectOrientatedProgramming.Chapter03.SingletonPattern;
+
+public class SingletonClass {
+    public static void main(String[] args) {
+        SingletonClass s1 = SingletonClass.getInstance();
+        SingletonClass s2 = SingletonClass.getInstance();
+
+        if (s1 != s2)
+            System.out.println("The objects are the same");
+        else
+            System.out.println("The objects are not the same");
+    }
+
+    private static SingletonClass instance;
+
+    private SingletonClass() {
+    }
+
+    public static SingletonClass getInstance() {
+        if (instance == null)
+            instance = new SingletonClass();
+        return instance;
+    }
+}
