6ccaa1b1ab881c6e0703906afb8f4ce9a6c527d5
ConradMar3 <conradmare890317@gmail.com>
2022-08-07 09:20:06 +0200

    2022/08/07 (am)
    Book3 - Chapter07.
    Using Static Classes, Using Anonymous Classes.

---
+++ JavaOCP2_All.In.One/src/Book03_ObjectOrientatedProgramming/Chapter07/UsingAnonymousInnerClasses_page347/CreatingAnAnonymousClass/AnonClass.java
@@ -0,0 +1,28 @@
+package Book03_ObjectOrientatedProgramming.Chapter07.UsingAnonymousInnerClasses_page347.CreatingAnAnonymousClass;
+
+/*
+The basic form for declaring and instantiating an anonymous class is this:
+
+    new ClassOrInterface() { class-body }
+
+1. new keyword followed by the name of a class or interface that specifies
+   the type of the object created from the anonymous class.
+
+2. This class or interface name is followed by parentheses, which may include a parameter
+   list that’s passed to the constructor of the anonymous class.
+ */
+
+public class AnonClass {
+    public static void main(String[] args) {
+        Ball b = new Ball() {
+            public void hit() {
+                System.out.println("You hit it!");
+            }
+        };
+        b.hit();
+    }
+
+    interface Ball {
+        void hit();
+    }
+}
