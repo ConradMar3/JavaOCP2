ee924d4808139a84d9135647678cb02c8002b8d0
ConradMar3 <conradmare890317@gmail.com>
2022-08-08 09:30:53 +0200

    2022/08/08 (am)
    Book3 - Chapter08.
    Classes, Own Packages.

---
+++ JavaOCP2_All.In.One/src/com/gmail/conradmare890317/util/PackageTest.java
@@ -0,0 +1,11 @@
+package com.gmail.conradmare890317.util;
+
+public class PackageTest {
+    public static void main(String[] args)
+    {
+        while (Console.askYorN("Keep going?"))
+        {
+            System.out.println("D'oh!");
+        }
+    }
+}
