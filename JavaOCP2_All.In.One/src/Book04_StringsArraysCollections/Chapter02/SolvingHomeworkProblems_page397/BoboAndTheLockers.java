d2b29ceeb85baa7a04f9f2fefd70ad7279225197
ConradMar3 <conradmare890317@gmail.com>
2022-08-15 09:49:41 +0200

    2022/08/15 (am)
    Homework problem.

--- JavaOCP2_All.In.One/src/Book04_StringsArraysCollections/Chapter02/SolvingHomeworkProblems_page397/BoboAndTheLockers.java
+++ JavaOCP2_All.In.One/src/Book04_StringsArraysCollections/Chapter02/SolvingHomeworkProblems_page397/BoboAndTheLockers.java
@@ -1,5 +1,18 @@
 package Book04_StringsArraysCollections.Chapter02.SolvingHomeworkProblems_page397;
 
+
+/*
+Bobo (these problems always had a character named Bobo in them) visits the local
+high school on a Saturday and finds that all the school’s 1,000 lockers are neatly
+closed. So he starts at one end of the school and opens them all. Then he goes
+back to the start and closes every other locker (lockers 2, 4, 6, and so on). Then he
+goes back to the start and hits every third locker: If it’s open, he closes it; if it’s
+closed, he opens it. Then he hits every fourth locker, every fifth locker, and so on.
+He keeps doing this all weekend long, walking the hallways opening and closing
+lockers 1,000 times. Then he gets bored and goes home. How many of the school’s
+1,000 lockers are left open, and which ones are they?
+ */
+
 public class BoboAndTheLockers {
     public static void main(String[] args) {
 // true = open; false = closed
