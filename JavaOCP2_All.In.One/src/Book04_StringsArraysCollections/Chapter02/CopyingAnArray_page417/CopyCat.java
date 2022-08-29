da55e05992484e055ff2eeaf95bf05644cd04b96
ConradMar3 <conradmare890317@gmail.com>
2022-08-29 11:39:01 +0200

    2022/08/29 (am)
    Book4 - Chapter02.
    Copying Arrays, Sorting Arrays.

---
+++ JavaOCP2_All.In.One/src/Book04_StringsArraysCollections/Chapter02/CopyingAnArray_page417/CopyCat.java
@@ -0,0 +1,31 @@
+package Book04_StringsArraysCollections.Chapter02.CopyingAnArray_page417;
+
+import java.util.Arrays;
+
+public class CopyCat {
+    public static void main(String args[]) {
+        int arrayOriginal[] = {42, 55, 21};
+        int arrayNew[] =
+                Arrays.copyOf(arrayOriginal, 3);
+        printIntArray(arrayNew);
+    }
+
+    static void printIntArray(int arrayNew[]) {
+        for (int i : arrayNew) {
+            System.out.print(i);
+            System.out.print(' ');
+        }
+        System.out.println();
+    }
+}
+
+/*
+The copyOfRange method is even more versatile. If you execute the instruction's:
+
+> int arrayOriginal[] = {42, 55, 21, 16, 100, 88};
+> int arrayNew[] = Arrays.copyOfRange(arrayOriginal, 2, 5);
+
+the values in arrayNew are:
+
+21 16 100
+ */
