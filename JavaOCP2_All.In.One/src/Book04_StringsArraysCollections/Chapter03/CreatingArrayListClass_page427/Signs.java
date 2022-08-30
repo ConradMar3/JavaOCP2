88014beac3147d5a49f46009b2d4bf5a4e5b05d9
ConradMar3 <conradmare890317@gmail.com>
2022-08-30 10:57:26 +0200

    2022/08/30 (am)
    Book4 - Chapter03.
    Creating Array List Class.

---
+++ JavaOCP2_All.In.One/src/Book04_StringsArraysCollections/Chapter03/CreatingArrayListClass_page427/Signs.java
@@ -0,0 +1,20 @@
+package Book04_StringsArraysCollections.Chapter03.CreatingArrayListClass_page427;
+
+import java.util.ArrayList;
+
+public class Signs {
+    public static void main(String[] args) {
+        // Multi line Array method:
+        //ArrayList signs;
+        //signs = new ArrayList();
+
+        // Single line:
+        // > ArrayList signs = new ArrayList();
+
+        ArrayList sign = new ArrayList(100);
+
+        // ArrayList that holds a String Object:
+        ArrayList<String> sign1 = new ArrayList<String>();
+
+    }
+}
