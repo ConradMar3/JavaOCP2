cd735db8540e310dbde39517bb49a828c0b0e39a
ConradMar3 <conradmare890317@gmail.com>
2022-09-04 13:34:50 +0200

    2022/09/04 (pm)
    Book4 - Chapter05.
    Wildcard's, Generic Class, Generic Queue Class.

---
+++ JavaOCP2_All.In.One/src/Book04_StringsArraysCollections/Chapter05_CreatingGenericCollectionClasses/GenericQueueClass_page458/Employee.java
@@ -0,0 +1,18 @@
+package Book04_StringsArraysCollections.Chapter05_CreatingGenericCollectionClasses.GenericQueueClass_page458;
+
+class Employee {
+    public String lastName;
+    public String firstName;
+
+    public Employee() {
+    }
+
+    public Employee(String last, String first) {
+        this.lastName = last;
+        this.firstName = first;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
