75762cddd4a657a16fde18d9df4f9ae93e2258e4
ConradMar3 <conradmare890317@gmail.com>
2022-09-05 09:42:03 +0200

    2022/09/05 (am)
    Book4 - Chapter06.
    Bulk Data Operation Types.

---
+++ JavaOCP2_All.In.One/src/Book04_StringsArraysCollections/Chapter6_BulkDataOperations/BasicBulkData_page464/Spell.java
@@ -0,0 +1,52 @@
+package Book04_StringsArraysCollections.Chapter6_BulkDataOperations.BasicBulkData_page464;
+
+import java.util.ArrayList;
+
+class Spell {
+    public String name;
+    public SpellType type;
+    public String description;
+
+    public enum SpellType {SPELL, CHARM, CURSE}
+
+    public Spell(String spellName, SpellType spellType,
+                 String spellDescription) {
+        name = spellName;
+        type = spellType;
+        description = spellDescription;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public class Spells {
+        public void main(String[] args) {
+
+            ArrayList<Spell> spells = new ArrayList<>();
+            spells.add(new Spell("Aparecium", Spell.SpellType.SPELL, "Makes invisible ink appear."));
+            spells.add(new Spell("Avis", Spell.SpellType.SPELL, "Launches birds from your wand."));
+            spells.add(new Spell("Engorgio", Spell.SpellType.CHARM, "Enlarges something."));
+            spells.add(new Spell("Fidelius", Spell.SpellType.CHARM, "Hides a secret within someone."));
+            spells.add(new Spell("Finite Incatatum", Spell.SpellType.SPELL, "Stops all current spells."));
+            spells.add(new Spell("Locomotor Mortis", Spell.SpellType.CURSE, "Locks an opponent's legs."));
+
+            for (Spell spell : spells)
+                System.out.println(spell.name);
+            spells.stream().forEach(s -> System.out.println(s));
+
+            for (Spell spell : spells) {
+                if (spell.type == Spell.SpellType.SPELL)
+                    System.out.println(spell.name);
+            }
+            spells.stream()
+                    .filter(s -> s.type == Spell.SpellType.SPELL)
+                    .forEach(s -> System.out.println(s));
+
+            spells.parallelStream()
+                    .filter(s -> s.type == Spell.SpellType.SPELL)
+                    .filter(s -> s.name.toLowerCase().startsWith("a"))
+                    .forEach(s -> System.out.println(s));
+        }
+    }
+}
---
+++ JavaOCP2_All.In.One/src/Book04_StringsArraysCollections/Chapter6_BulkDataOperations/StreamInterface_page467/Stream.java
@@ -0,0 +1,18 @@
+package Book04_StringsArraysCollections.Chapter6_BulkDataOperations.StreamInterface_page467;
+
+public class Stream {
+    /*
+    The Stream interface defines about 40 methods. In addition, three related
+    interfaces — DoubleStream, IntStream, and LongStream — extend the Stream
+    interface to define operations that are specific to a single date type: double, int,
+    and long. Table 6-1 lists the most commonly used methods of these interfaces.
+
+    The filter method is one of the most commonly used stream methods. It’s argument,
+    called a predicate, is a function that returns a boolean value. The function is
+    called once for every element in the stream and is passed a single argument that
+    contains the element under question. If the method returns true, the element is
+    passed on to the result stream. If it returns false, the element is not passed on.
+
+
+     */
+}
